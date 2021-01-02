1bbb26b18a959d25ec0bf0530a541bb3d1838f40
Sumit Shitole <sumitshitole1000@gmail.com>
2021-01-02 09:53:17 +0530

    Calling sp using code first approach

--- EFDemo/Repository/EmployeeRepositoryCF.cs
+++ EFDemo/Repository/EmployeeRepositoryCF.cs
@@ -21,28 +21,59 @@ namespace EFDemo.Repository
 
         internal void InvokeStoreProcedure()
         {
-           using(EmployeeDBContextCF DB=new EmployeeDBContextCF())
+            using (EmployeeDBContextCF DB = new EmployeeDBContextCF())
             {
-                SqlParameter DepartmentID = new SqlParameter("@DepartmentID", 2);
 
+                // Calling sp with single parameter and returning model
+                SqlParameter DepartmentID = new SqlParameter("@DepartmentID", 2);
                 var department = DB.Departments
                     .SqlQuery("spGetDepartmentDetails @DepartmentID", DepartmentID)
                     .FirstOrDefault();
 
+                //calling sp without parameter and returning AnonymousType
                 var AnonymousType = DB.Database
                     .SqlQuery<spGetanonymousType_ResultType>("spGetanonymousType")
                     .ToList();
 
-                var ScalarValue = DB.Database
+                //Calling sp without parameter and returning string value
+                var ScalarValueString = DB.Database
                     .SqlQuery<string>("spGetScalarValue")
                     .FirstOrDefault();
-                var Params = new SqlParameter()[];
+
+                //Calling sp without parameter and returning int value
+                var ScalarValueInt = DB.Database
+                    .SqlQuery<int>("spGetScalarValueInt")
+                    .FirstOrDefault();
+
+
+
 
                 //pass multiple parameter
-                var AffectedRows = DB.Database.ExecuteSqlCommand<int>("spCreateEmployee @FirstName @LastName @Gender @Salary @DepartmentID",
-                    new SqlParameter("@FirstName", "Sumit");
+                SqlParameter FirstName = new SqlParameter("@FirstName", "Sumit");
+                SqlParameter LastName = new SqlParameter("@LastName", "Shitole");
+                SqlParameter Gender = new SqlParameter("@Gender", "Male");
+                SqlParameter Salary = new SqlParameter("@Salary", "5000");
+                SqlParameter DepartmentId = new SqlParameter("@DepartmentID", "2");
 
+                //var AffectedRows = DB.Database.SqlQuery<int>("spCreateEmployee @FirstName @LastName @Gender @Salary @DepartmentID",
+                //    parameters.ToArray());
 
+                //This Worked fine
+                var AffectedRows = DB.Database.SqlQuery<int>("EXEC spCreateEmployee @FirstName,@LastName,@Gender,@Salary,@DepartmentID",
+                    new object[] { FirstName, LastName, Gender, Salary, DepartmentId }).ToList();
+
+                string CommandText = "spCreateEmployee @FirstName=@FirstName_Param,@LastName=@LastName_Param,";
+                CommandText += "@Gender=@Gender_Param,@Salary=@Salary_Param,@DepartmentID=@DepartmentID_Param";
+                var sqlparams = new[]
+                {
+                new SqlParameter("@FirstName_Param","SumitNew"),
+                new SqlParameter("@LastName_Param", "ShitoleNew"),
+                new SqlParameter("@Gender_Param", "GenderNew"),
+                new SqlParameter("@Salary_Param", "55555"),
+                new SqlParameter("@DepartmentID_Param", "3")
+                };
+
+                var AffectedRowsNew = DB.Database.SqlQuery<int>(CommandText, sqlparams).FirstOrDefault();
             }
         }
     }
